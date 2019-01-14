2ec12ac1a151546a0008ebac0651f3361117ebed
Yousif Kzeer <kzeery@mcmaster.ca>
2019-01-13 22:45:13 -0500

    Seventh Day Learning Web Dev

---
+++ Learning Javascript/forLoops.js
@@ -0,0 +1,17 @@
+for(var i = -10; i < 20; i++) {
+    console.log(i);
+}
+console.log("Next Problem");
+for(var i = 10; i < 41; i +=2) {
+    console.log(i);
+}
+console.log("Next Problem");
+for(var i = 301; i < 334; i += 2) {
+    console.log(i);
+}
+console.log("Next Problem");
+for(var i = 5; i < 51; i += 5) {
+    if(i%3 === 0) {
+        console.log(i);
+    }
+}
