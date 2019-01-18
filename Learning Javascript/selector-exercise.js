3a019d242cf387836ceb919dee0d52af11122df2
Yousif Kzeer <kzeery@mcmaster.ca>
2019-01-17 21:22:35 -0500

    Ninth Day Learning Web Dev

--- Learning Javascript/selector-exercise.js
+++ Learning Javascript/selector-exercise.js
@@ -2,3 +2,9 @@ var first = document.querySelector("p");
 var second = document.getElementsByTagName("p")[0];
 var third = document.querySelector("#first");
 var fourth = document.querySelector(".special");
+var h1 = document.querySelector("h1");
+h1.addEventListener("click", function() {
+    alert("h1 was clicked!");
+    h1.style.background = "orange";
+})
+
