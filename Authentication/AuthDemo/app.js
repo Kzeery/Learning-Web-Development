20e212be9d45e6b2b3ff49fc01fbe6c4f17d2705
Yousif Kzeer <kzeery@mcmaster.ca>
2019-02-18 17:22:27 -0500

    Sixteenth Day Learning Web Dev

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,24 +1,81 @@
 const   passportLocalMongoose = require("passport-local-mongoose"),
         LocalStrategy         = require("passport-local"),
+        User                  = require("./models/user"),
         bodyParser            = require("body-parser"),
         mongoose              = require("mongoose"),
         passport              = require("passport"),
-        express               = require("express"),  
+        express               = require("express"),
         app                   = express(),
         hostname              = "127.0.0.1",
         port                  = 3000;
-
+        
 mongoose.set("useNewUrlParser", true);
-mongoose.connect("mongodb://localhost/auth_demo_app");
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/auth_demo_app");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Cynthia",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// =============
+// ROUTES
+// =============
 
 app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
     res.render("secret");
 });
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secret");
+        });
+    });
+});
+
+// LOGIN ROUTES
+// render login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res) {
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(port, hostname, function() {
     console.log("App has started!");
